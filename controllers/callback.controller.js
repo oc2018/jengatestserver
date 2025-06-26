0bc31a5715205308f68c98baa9434e481f33764e
eric <ontime.c23@gmail.com>
2025-06-26 13:19:36 +0300

    modified:   controllers/callback.controller.js

--- controllers/callback.controller.js
+++ controllers/callback.controller.js
@@ -1,4 +1,13 @@
 export const getCallback = (req, res) => {
+  const payload = req.body;
+  try {
+    console.log("Jenga callback received", payload);
+
+    res.status(200).send("OK");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "response error" });
+  }
   console.log(response);
   res.send(response.data);
 };
