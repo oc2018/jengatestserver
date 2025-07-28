a00ee9c0351cc3a387153a0c704e9b872eb4c77f
eric <ontime.c23@gmail.com>
2025-07-28 15:27:49 +0300

    modified:   routes/users.routes.js

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -12,9 +12,9 @@ import auth from "../middleware/index.js";
 const router = express.Router();
 
 router.get("/", auth, getUsers);
+router.get("/:id", auth, getUser);
 router.post("/signin", login);
 router.post("/signup", createUser);
-router.get("/:id", auth, getUser);
 router.patch("/:id", auth, updateUser);
 router.delete("/:id", auth, deleteUser);
 
